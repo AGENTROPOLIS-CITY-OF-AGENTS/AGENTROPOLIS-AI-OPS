9ba00f5d72aba346ec7016425b21f9f9fdac7fd6
Wired🧠Chaos <chaoswired@gmail.com>
2026-08-08 12:32:39 -0700

    feat: expose deterministic NumPy metrics

---
+++ src/agentropolis_ai_ops/__init__.py
@@ -0,0 +1,21 @@
+from .metrics import (
+    CouncilScoreResult,
+    DriftResult,
+    EntropyResult,
+    RollingStatsResult,
+    council_weighted_score,
+    drift_distance,
+    normalized_entropy,
+    rolling_stats,
+)
+
+__all__ = [
+    "CouncilScoreResult",
+    "DriftResult",
+    "EntropyResult",
+    "RollingStatsResult",
+    "council_weighted_score",
+    "drift_distance",
+    "normalized_entropy",
+    "rolling_stats",
+]
